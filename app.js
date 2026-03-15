4cad72d3e2b8e680874b7e6c341172db6d00e734
AyushKumar000 <ayushakr002@gmail.com>
2026-03-15 16:26:08 +0530

    change the api to send msg to frontend

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config'
 
 const app = express();
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js server!');
+    res.status(200).json({msg : "Hey Guys! Ayush This Side...."});
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
